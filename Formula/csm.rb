825dc730cfc20ebcf7a58588a9950e2d502529b6
welcomra1n <welcom2rain@gmail.com>
2026-06-17 00:41:03 +0000

    Update csm to v0.3.3

--- Formula/csm.rb
+++ Formula/csm.rb
@@ -7,20 +7,20 @@ class Csm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/welcomra1n/session-manager-tui/releases/download/v0.3.3/csm-darwin-arm64"
-      sha256 "0ce7a954ead1c04b56be0ac1432ec3dd488ed08b0b4ce958c01acb48b52cd63f"
+      sha256 "fa4dbb30e08fad258a1a96f70eea771121e2bcc389130251e4c15e4790d95532"
     else
       url "https://github.com/welcomra1n/session-manager-tui/releases/download/v0.3.3/csm-darwin-amd64"
-      sha256 "b3f4870f51e98a1aea220e8c7bd5a64a1ba8110b7df0964c35b838ad3991dd19"
+      sha256 "c72c53b309ebf3375eabd0fac069bafae5a2c2ec4b7fac43e844f9fa5b1e7688"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/welcomra1n/session-manager-tui/releases/download/v0.3.3/csm-linux-arm64"
-      sha256 "66fdb0f75c0f6fe9c29ba6a9aa7012837ea603912e12540ee6f429cf31aa7be6"
+      sha256 "c551bb9cc2c8194853a25f401ce1cee1e753f901bdc9c2fa71a85f39104df25d"
     else
       url "https://github.com/welcomra1n/session-manager-tui/releases/download/v0.3.3/csm-linux-amd64"
-      sha256 "6e3c655a29acbc85470f3b2ea2733b834e7760601b6476ca4ce8c7b1f84e0223"
+      sha256 "2dc10327eb94179f8f934892b19030410a1864b80f1c6faa28eb1c10bd2afbc1"
     end
   end
 
